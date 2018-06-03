08adb99e3e07d6234de64f827433c76775688c18
MarosAware <marosdevone@gmail.com>
2018-06-03 16:38:08 +0200

    DB file fixed.

--- DB/BOK_Git.sql
+++ DB/BOK_Git.sql
@@ -15,6 +15,12 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+DROP Database if EXISTS BOK;
+CREATE Database IF NOT EXISTS BOK;
+
+use BOK;
+
 --
 -- Table structure for table `conversation`
 --
